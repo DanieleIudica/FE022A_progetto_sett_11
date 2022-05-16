d6bc74212d847abe6b0b1347f36fa759403015d4
DanieleIudica <daniele.iudica@hotmail.it~>
2022-05-16 20:41:29 +0200

    risolto errore dovuto a delay e aggiunto spinner durante loading

--- src/app/components/details.component.ts
+++ src/app/components/details.component.ts
@@ -1,22 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { Product } from '../interface/product'; //interface
-import { CartService } from '../services/cart.service'; //services
-import { ProductsService } from '../services/products.service'; //services
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { Product } from "../interface/product"; //interface
+import { CartService } from "../services/cart.service"; //services
+import { ProductsService } from "../services/products.service"; //services
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-details',
+  selector: "app-details",
   template: `
     <div class="card mx-auto mt-4 " style="width: 18rem;">
       <h3 class="text-center">Dettagli Prodotto:</h3>
-      <div class="card-body">
+      <div *ngIf="loading" class="spinner-border d-flex mx-auto" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+      <div *ngIf="myProd != null" class="card-body">
         <img src="{{ myProd.prod_url }}" class="card-img-top" />
         <h5 class="card-title">{{ myProd.name }}</h5>
-        <p class="card-text">{{ myProd.price | currency: 'EUR' }}</p>
+        <p class="card-text">{{ myProd.price | currency: "EUR" }}</p>
         <p class="card-text">{{ myProd.description }}</p>
-        <button class="btn btn-dark" (click)="addToCart()">
-          Aggiungi al carrello
-        </button>
+        <button class="btn btn-dark" (click)="addToCart()">Aggiungi al carrello</button>
       </div>
     </div>
   `,
@@ -24,18 +25,17 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class DetailsComponent implements OnInit {
   myProd!: Product;
+  loading: boolean = false; // for the spinner
 
-  constructor(
-    private prodSrv: ProductsService,
-    private cartSrv: CartService,
-    private router: ActivatedRoute
-  ) {}
+  constructor(private prodSrv: ProductsService, private cartSrv: CartService, private router: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.loading = true;
     this.router.params.subscribe(async (params) => {
-      const id = +params['id'];
+      const id = +params["id"];
       this.prodSrv.getDetails(id).subscribe((prod) => {
         this.myProd = prod;
+        this.loading = false;
       });
     });
   }
--- src/app/components/home.component.ts
+++ src/app/components/home.component.ts
@@ -1,30 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { Product } from '../interface/product';
-import { ProductsService } from '../services/products.service';
+import { Component, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
+import { Product } from "../interface/product";
+import { ProductsService } from "../services/products.service";
 
 @Component({
-  selector: 'app-home',
+  selector: "app-home",
   template: `
     <div class="container mt-2">
       <h1 class="text-center">Simulazione Ecommerce</h1>
       <div class="d-flex flex-md-row flex-column flex-wrap mt-4 mx-5 gap-2">
-        <div
-          class="card"
-          style="width: 18rem;"
-          *ngFor="let item of prodList; let i = index"
-        >
+        <div *ngIf="loading" class="spinner-border d-flex mx-auto" role="status">
+          <span class="visually-hidden">Loading...</span>
+        </div>
+        <div class="card" style="width: 18rem;" *ngFor="let item of prodList; let i = index">
           <img src="{{ item.prod_url }}" class="card-img-top" />
           <div class="card-body">
             <h5 class="card-title">{{ item.name }}</h5>
             <p class="card-text">{{ item.description }}</p>
 
-            <a
-              [routerLink]="['products/', item.id]"
-              type="button"
-              class="btn btn-dark"
-              >Dettagli</a
-            >
+            <a [routerLink]="['products/', item.id]" type="button" class="btn btn-dark">Dettagli</a>
           </div>
         </div>
       </div>
@@ -35,14 +29,16 @@ import { ProductsService } from '../services/products.service';
 export class HomeComponent implements OnInit {
   // products list, type of Product
   prodList: Product[] | undefined;
-
+  loading: boolean = false;
   sub!: Subscription;
 
   constructor(private prodSrv: ProductsService) {}
 
   ngOnInit(): void {
+    this.loading = true;
     this.sub = this.prodSrv.getProducts().subscribe((list) => {
       this.prodList = list;
+      this.loading = false;
     });
   }
 }
